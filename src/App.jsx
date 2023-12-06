055182d80980d221745a12488a6c27caf9499b34
algorithm_artisit <algorithm_artist@DESKTOP-SV23GD5>
2023-12-06 06:38:14 -0800

    got players to show up in arrays, need to set up routes for details and search page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css'
 import PuppyList from "./PuppyList"
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
 
@@ -8,6 +9,26 @@ function App() {
 <>
 <h1>Puppy Details</h1>
 <PuppyList/>
+<div id="container">
+      <h1></h1>
+      <div id="navbar">
+        <Link to ="/"></Link>
+        <Link to ="/"></Link>
+        <Link to ="/"></Link>
+
+        </div>
+      <div id="main-section">
+          <Routes>
+          <Route path="/" element ={<h1></h1>} />
+          <Route path="/" element ={<h1></h1>} />
+          <Route path="/" element ={<h1></h1>} />
+          </Routes></div>
+    </div>
+
+
+
+
+
 </>
   );
 
--- src/PuppyList.jsx
+++ src/PuppyList.jsx
@@ -3,9 +3,9 @@ import { useEffect } from "react";
 const PuppyList = () => {
     useEffect(() =>{
         const getPuppies = async() => {
-        const response = await fetch (` https://fsa-puppy-bowl.herokuapp.com/api/`);
+        const response = await fetch (`https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players`);
            const responseJson = await response.json();
-        console.log(responseJson);
+        console.log(responseJson.data.players);
         };
         getPuppies();
     })

2f731308089bb1f49b0ef49c838a40bd61f51c50
algorithm_artisit <algorithm_artist@DESKTOP-SV23GD5>
2023-12-06 08:04:27 -0800

    Rendered Puppy Names on front-end.

--- src/PuppyList.jsx
+++ src/PuppyList.jsx
@@ -1,16 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const PuppyList = () => {
-    useEffect(() =>{
-        const getPuppies = async() => {
-        const response = await fetch (`https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players`);
-           const responseJson = await response.json();
-        console.log(responseJson.data.players);
+    const [AllPuppyList, setAllPuppyList] = useState([]);
+    useEffect(() => {
+        const getPuppies = async () => {
+            const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/players`);
+            const responseJson = await response.json();
+            const allPuppies = responseJson.data.players
+            console.log(allPuppies);
+            setAllPuppyList(allPuppies);
+            
+            
         };
         getPuppies();
-    })
-    return(
-        <h1>Puppy List Works!</h1>
+    }, [])
+    return (
+      <>
+            <h1>Puppy List Works!</h1>
+            {
+                AllPuppyList.map((singlePuppy) => { 
+                console.log(singlePuppy);
+                return <li>{singlePuppy.name}</li>
+                })
+            }
+        </>
     )
 }
-    export default PuppyList;
+export default PuppyList;
